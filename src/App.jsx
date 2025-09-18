6d808b2a5e6b5989cbba80086a07bc84a9d987d1
incrediblecollection0-netizen <khuram477@gmail.com>
2025-09-18 06:38:58 +0500

    the

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import viteLogo from '/vite.jpg';
 import './App.css';
 function App() {
 
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Inkredible Collection</h1>
     </>
   );
 }
